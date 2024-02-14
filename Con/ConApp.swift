f00d6f05358d2a55099f2e0d05ee60a602d640cd
Espe13 <42141549+Espe13@users.noreply.github.com>
2024-02-14 12:54:56 +0000

    Initial Commit

---
+++ Con/ConApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConApp.swift
+//  Con
+//
+//  Created by Amanda on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ConApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
